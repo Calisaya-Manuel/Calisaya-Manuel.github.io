c55b2e6eb566764381bf6ef6ce18b2494f40def5
Calisaya <102339222+Calisaya-Manuel@users.noreply.github.com>
2023-10-25 23:50:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var titulo = document.getElementById("titulo");
+
+titulo.addEventListener("click", function() {
+    window.location.href = "https://www.instagram.com/progra_c4m/"; // Reemplaza con la URL de la página a la que deseas redirigir
+});
